2f418c2e3329b1a56b3dff1db4437c5f4fa798d4
Andy Ball <932400399@bun2-204-14.local>
2019-06-07 16:37:55 +1200

    logic for viewday tasks() working; need to turn mondayItem into html

--- js/main.js
+++ js/main.js
@@ -2,14 +2,6 @@ $(document).ready(function(){
 
   var tasks = [];
 
-  // var viewMonBtn = $('#viewMonBtn');
-  // var viewTuesBtn = $('#viewTuesBtn');
-  // var viewWedBtn = $('#viewWedBtn');
-  // var viewThursBtn = $('#viewThursBtn');
-  // var viewFriBtn = $('#viewFriBtn');
-  //
-  // check your phone for the view by day buttons - passing this.text()
-
   $('#addTaskBtn').click(function(){
     $('#addTaskOverlay').show();
     $('.close-button').click(function(){
@@ -73,55 +65,117 @@ $(document).ready(function(){
       $('#viewAllTasksOverlay').hide();
     });
 
+    document.getElementById('viewAllTaskItems').innerHTML = '';
+
     for (var i = 0; i < tasks.length; i++) {
 
-      var taskList = $('#viewAllTaskItems');
+    var urgentClass;
+    var urgentText;
+      if (tasks[i].urgent === true) {
+        urgentClass = 'urgent-text';
+        urgentText = 'urgent!'
+      } else {
+        urgentClass = 'non-urgent-text'
+        urgentText = 'not urgent'
+      }
 
-      var urgentClass;
-      var urgentText;
-        if (tasks[i].urgent === true) {
-          urgentClass = 'urgent-text';
-          urgentText = 'urgent!'
-        } else {
-          urgentClass = 'non-urgent-text'
-          urgentText = 'not urgent'
-        }
-      // need to get this stuff rendering !
-      // 
+    var taskList = '<div class="row">';
+      taskList += '<div class="col-12 mb-1">';
+        taskList += '<h4 class="text-light">' + tasks[i].name + '</h4>';
+      taskList += '</div>';
+    taskList += '</div>';
 
-      taskList.innerHTML += '<div class="row">';
-        taskList.innerHTML += '<div class="col-12 mb-1">';
-          taskList.innerHTML += '<h4 class="text-light">' + tasks[i].name + '</h4>';
-        taskList.innerHTML += '</div>';
-      taskList.innerHTML += '</div>';
+    taskList += '<div class="row mb-1">';
+      taskList += '<div class="col-8">';
+        taskList += '<p class="text-light"> ' + tasks[i].description + '</p>'
+      taskList += '</div>';
+    taskList += '</div>';
 
-      taskList.innerHTML += '<div class="row mb-1">';
-        taskList.innerHTML += '<div class="col-8">';
-          taskList.innerHTML += '<p class="text-light"> ' + tasks[i].description + '</p>'
-        taskList.innerHTML += '</div>';
-      taskList.innerHTML += '</div>';
+    taskList += '<div class="row mb-1">';
+      taskList += '<div class="col-8">';
+        taskList += '<p class="text-light">due <strong>' + tasks[i].dueDate + '</strong></p>';
+      taskList += '</div>';
+    taskList += '</div>';
 
-      taskList.innerHTML += '<div class="row mb-1">';
-        taskList.innerHTML += '<div class="col-8">';
-          taskList.innerHTML += '<p class="text-light">due <strong>' + tasks[i].dueDate + '</strong></p>';
-        taskList.innerHTML += '</div>';
-      taskList.innerHTML += '</div>';
+    taskList += '<div class="row mb-1">';
+      taskList += '<div class="col-8">';
+        taskList += '<p class="text-light">' + tasks[i].category + '</p>'
+      taskList += '</div>';
+    taskList += '</div>';
 
-      taskList.innerHTML += '<div class="row mb-1">';
-        taskList.innerHTML += '<div class="col-8">';
-          taskList.innerHTML += '<p class="text-light">' + tasks[i].category + '</p>'
-        taskList.innerHTML += '</div>';
-      taskList.innerHTML += '</div>';
+    taskList += '<div class="row mb-3">';
+      taskList += '<div class="col-8">';
+        taskList += '<p class="' +  urgentClass + '">this task is ' +  urgentText + '</p>'
+      taskList += '</div>';
+    taskList += '</div>';
 
-      taskList.innerHTML += '<div class="row mb-3">';
-        taskList.innerHTML += '<div class="col-8">';
-          taskList.innerHTML += '<p class="' +  urgentClass + '">this task is ' +  urgentText + '</p>'
-        taskList.innerHTML += '</div>';
-      taskList.innerHTML += '</div>';
+    document.getElementById('viewAllTaskItems').innerHTML += taskList;
 
     }
 
   });
+
+  $('.viewDayTasks').click(function(){
+
+    if (this.innerText === 'M'){
+      $('#viewDayTasksOverlay').show();
+
+      $('.close-button').click(function(){
+        $('#viewDayTasksOverlay').hide();
+      });
+
+      $('#viewDayTasksHeader').html('tasks for Monday:');
+
+      document.getElementById('viewDayTasksItems').innerHTML = '';
+
+      for (var i = 0; i < tasks.length; i++) {
+
+        var urgentClass;
+        var urgentText;
+          if (tasks[i].urgent === true) {
+            urgentClass = 'urgent-text';
+            urgentText = 'urgent!'
+          } else {
+            urgentClass = 'non-urgent-text'
+            urgentText = 'not urgent'
+          }
+
+        var mondayItem;
+          if (tasks[i].dueDate === 'Monday'){
+            mondayItem = 'monday item';
+          }
+
+        document.getElementById('viewDayTasksItems').innerHTML += mondayItem;
+        // need to turn 'monday item' into html. 
+
+      }
+    } else if (this.innerText === 'T'){
+      console.log('got a tues click');
+    } else if (this.innerText === 'W'){
+      console.log('got a wed click');
+    } else if (this.innerText === 'T'){
+      console.log('got a thurs click');
+    } else if (this.innerText === 'F'){
+      console.log('got a fri click');
+    }
+
+
+
+
+
+
+
+
+
+
+
+  });
+
+
+
+
+
+
 
   // show category buttons (loop, look for a category, break when you find it)
 
